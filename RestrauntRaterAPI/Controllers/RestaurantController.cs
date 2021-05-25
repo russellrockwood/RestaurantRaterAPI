dda4cc3cec48ba5a9d897143d14eacdf56be074d
Russell Rockwood <russell.rockwood@gmail.com>
2021-05-25 12:05:32 -0400

    Poco, dbcontext, controller, etc.

---
+++ RestrauntRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,31 @@
+﻿using RestrauntRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestrauntRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Request body can not be empty.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestrauntRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestrauntRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+        public bool IsRecommended => Rating > 3.5;
+    }
+}
